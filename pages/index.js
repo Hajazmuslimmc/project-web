5723f063600b106f5feb90d7b01d01e977553a61
Hajazmuslimmc <your-email@example.com>
2025-10-21 20:14:50 -0700

    Add basic pages/index.js home page

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to Web Games</h1>
+      <p>Get ready to play amazing games!</p>
+    </div>
+  );
+}
+
+export default Home;
